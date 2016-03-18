58a24a893a701fb9bfda6b6914ebf4b1d5fbac5f
John Skilbeck <john.skilbeck@gmail.com>
2016-03-18 15:13:36 -0700

    mpr base

--- sql/load_mpr_base.sql
+++ sql/load_mpr_base.sql
@@ -1,4 +1,15 @@
+
+declare @now date, @start date, @end date 
+
+set @now = getdate()
+set @start = dateadd(mm,(year(@now)- 1900) * 12 + month(@now) - 1 -1 , 0)
+set @end   = dateadd(d,-1 , dateadd(mm,(year(@now)- 1900) * 12 + month(@now)- 1 , 0))  
+
+
 select 
 	*
 from 
 	ETLStaging..FinanceBaseMPR MPR
+where
+	Date between @start and @end
+
