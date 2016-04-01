10d8feb42886330cb3840bf9d42bcba456d5543d
John Skilbeck <john.skilbeck@gmail.com>
2016-04-01 15:53:09 -0700

    think i fixed the date issue

--- jobs/MPR/mpr_base2.js
+++ jobs/MPR/mpr_base2.js
@@ -26,10 +26,10 @@ extract(source_db, file, subfolder, function(data){
   console.log(data);
 	transform(data, function(data){
     console.log(data);
-		// load(data, destination_db, sql, function(){
+		load(data, destination_db, sql, function(){
       // email(file, function(){
-      //   console.log('Data inserted.');        
-      // });
+        console.log('Data inserted.');        
+      });
 		// });
 	});
 });
