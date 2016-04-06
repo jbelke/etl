4f6f49c7d71f72571c25c68c8f4ffd08bc7b1d9a
John Skilbeck <john.skilbeck@gmail.com>
2016-04-06 16:30:09 -0700

    added mmf for commissions dependency

--- jobs/MPR/mmf.js
+++ jobs/MPR/mmf.js
@@ -15,12 +15,10 @@ var
   ;
 
 var sql = 'insert into mpr_base('+
-    ' Year, Month, Date, PlatformId, Gateway, Vertical, SoftwareName, ' +
-    ' ParentAccountId, ParentName, FeePaymentType, PaymentTypeGroup, Currency, ' +
-    ' TPV, TPV_Net, TPV_USD, TPV_Net_USD, TPV_Billing, Card_Volume_USD, Card_Volume_Net_USD, Txn_Count, ' +
-    ' Property_Fee, Net_Settled_Fee_USD, Convenience_Fee_USD, Revenue_USD, Revenue_Net_USD, Net_Settled_Fee_Net_USD, Convenience_Fee_Net_USD, ' + 
-    ' Credit_Card_USD, Debit_Card_USD, Amex_Processing_USD, Credit_Card_Net_USD, Debit_Card_Net_USD, Amex_Processing_Net_USD ' + 
-    ' ) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33)';
+    ' Date, PlatformId, Vertical, ' +
+    ' ParentAccountId, ParentName, ' +
+    ' MMF ' +
+    ' ) values ($1,$2,$3,$4,$5,$6)';
 
 extract(source_db, file, subfolder, function(data){
   // console.log(data);
