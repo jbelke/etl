d3b0c514b48574d3a9cb0a45803955fb795c7d25
John Skilbeck <john.skilbeck@gmail.com>
2016-04-15 11:29:41 -0700

    historical loads for aggregation tables

--- sql/Analytics/analytics.sql
+++ sql/Analytics/analytics.sql
@@ -3,8 +3,8 @@ declare @now as date, @start as date, @end as date,
 @PaymentTypeGroup as nvarchar(max), @Network as nvarchar(max)
 
 set @now = getdate()
-set @start = '2015-01-01'--'2016-01-01'--'2010-01-01'--dateadd(mm,(year(@now)- 1900) * 12 + month(@now) - 1 -1 , 0) 
-set @end = dateadd(d,-1 , dateadd(mm,(year(@now)- 1900) * 12 + month(@now)- 1 , 0))  
+set @start = '2010-01-01'--'2015-01-01'--'2016-01-01'--'2010-01-01'--dateadd(mm,(year(@now)- 1900) * 12 + month(@now) - 1 -1 , 0) 
+set @end = '2014-12-31'--dateadd(d,-1 , dateadd(mm,(year(@now)- 1900) * 12 + month(@now)- 1 , 0))  
 
 -- Temp Tables
 if object_id('tempdb..#PaymentTypeGroup') is not null drop table #PaymentTypeGroup
