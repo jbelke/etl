cf8167c51f98fea25cf386f8ac927dc9e0cd1cd7
John Skilbeck <john.skilbeck@gmail.com>
2016-04-29 11:21:46 -0700

    mpr

--- load.js
+++ load.js
@@ -12,7 +12,7 @@ var insert = function(data, destination_db, insert_sql, cb){
 		data.map(function(item,index){
 			destination_db.query(insert_sql, item, function(err,result){
 				console.log(err || result);
-				done();
+				// done();
 				// setTimeout(function(){
 				// 	destination_db.end();
 				// }, 1 * 60 * 1000);
